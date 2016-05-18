41d0d7f59f7d4bdafd2e98f63afcb5683e138965
Arnholz martin <arnholz.martin@gmail.com>
2016-05-18 14:05:16 +0200

    email add

--- src/main.c
+++ src/main.c
@@ -5,14 +5,15 @@
 ** Login : arnhol_m
 ** 
 ** Started on  Tue Jan  5 00:20:15 2016 
-** Last update Thu May 12 17:19:47 2016 
+** Last update Wed May 18 13:30:34 2016 
 */
 
 #include "wheeler.h"
 
 int	main(int ac, char **av)
 {
-  /*  if(ac == 4 && (getnbr(av[1]) >= 0) && getlen(av[2]) == 1 && av[3]){
+  //simply some test exemple
+  if(ac == 4 && (getnbr(av[1]) >= 0) && getlen(av[2]) == 1 && av[3]){
     affthat("first arg is an integer with value : %d \n", getnbr(av[1]));
     affthat("second arg is a char with value : %c \n", av[2][0]);
     affthat("third arg is a char star with value : %s \n", av[3]);
@@ -21,8 +22,6 @@ int	main(int ac, char **av)
   else{
     affthat("usage :\n -(int) ex >> 1234\n -(char)ex >> z\n -(char *) ex >> \"string\"\n");
     return (-1);
-    }*/
-  if (ac == 2)
-    affthat("%s \n", av[1]); 
+  }
   return (0);
 }
